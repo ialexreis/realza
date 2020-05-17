27c0b8e0c9e31e4600b40e28ac314b483c73b8bc
adevr <alex.cst.reis@gmail.com>
2020-05-17 23:17:59 +0100

    feat: new tab navigation

---
+++ components/TabNavigation.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import ColorScreen from '../screens/ColorScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+
+const Tab = new createBottomTabNavigator();
+
+export default function TabNavigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            if (route.name === 'ColorBlind') {
+              iconName = focused ? 'md-eye' : 'ios-eye';
+            } else if (route.name === 'Register') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!s
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}>
+        <Tab.Screen name="ColorBlind" component={ColorScreen} />
+        <Tab.Screen name="Register" component={RegisterScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
