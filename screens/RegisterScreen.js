875610ca8cc7d7635a8600811efd5755596c3908
adevr <alex.cst.reis@gmail.com>
2020-05-26 23:39:13 +0100

    refactor: class based component

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,10 +1,34 @@
 import * as React from 'react';
 import {Text, View} from 'react-native';
+import * as RNFS from 'react-native-fs';
 
-export default function RegisterScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Register Screen!</Text>
-    </View>
-  );
+class RegisterScreen extends React.Component{
+    constructor() {
+        super();
+        this.state.register = [ JSON.parse(this.getRegister()) ]
+    }
+
+    state = {
+        register: []
+    }
+
+    getRegister = () => {
+        let path = RNFS.DocumentDirectoryPath + '/palettes.json';
+        RNFS.readFile(path, 'utf8')
+            .then((contents) => {
+                return contents;
+            }).catch((err) => {
+            console.log(err.message);
+        });
+    };
+
+    render() {
+        return(
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <Text>Register Screen!</Text>
+            </View>
+        );
+    }
 }
+
+export default RegisterScreen;
